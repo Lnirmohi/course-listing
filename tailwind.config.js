f4ba85f3b59415cbb12e0971f59a1cb1dd7fdcab
Ubuntu <tester@techpearl.com>
2023-11-06 20:08:36 +0530

    add primary color and logo font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      'colors': {
+        'primary': '#232054'
+      },
+      fontFamily: {
+        RobotoSlab: ["Roboto Slab"]
+      }
+    },
   },
   plugins: [],
 }
