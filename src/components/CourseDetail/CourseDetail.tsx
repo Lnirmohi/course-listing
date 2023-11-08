93eea9f195af9cb96d72dd9ceaa4fa638f27af10
Ubuntu <tester@techpearl.com>
2023-11-08 18:46:15 +0530

    course list partial changes

--- src/components/CourseDetail/CourseDetail.tsx
+++ src/components/CourseDetail/CourseDetail.tsx
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function CourseDetail() {
-  return <div>CourseDetail</div>;
+  return (
+    <div className="w-full h-full">
+      <div className="p-4">
+        <p className="inline-block mb-2 font-bold text-xl text-slate-500 pb-1 border-b-2 border-slate-500">
+          Course List
+        </p>
+      </div>
+    </div>
+  );
 }
